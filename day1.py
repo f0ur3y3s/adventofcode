415c046c52c864dbcfaad27a05dc057bdb0839db
incub4t0r <dchung0812@gmail.com>
2021-12-01 08:45:03 -0500

    added day 1 part 2

--- day1.py
+++ day1.py
@@ -1,8 +1,13 @@
+# Day 1 part 1 and 2
+
 import os
 
 root = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 file_name = "day1.txt"
 target = os.path.join(root, file_name)
+
+# Part 1
+print("------------part 1------------")
 
 prev = -9999
 counter = 1
@@ -31,3 +36,17 @@ with open(target) as f:
 print(f"increasing: {increasing}\ndecreasing: {decreasing}\nno change: {nochange}")#, decreasing, nochange)
 # 1461 is too low
 # 1800? is too high
+# 1462
+
+# Part 2
+
+print("------------part 2------------")
+nums = [int(num.rstrip()) for num in open(target)]
+p2 = sum(1 for one, two, three, four in zip(nums, nums[1:], nums[2:], nums[3:]) if one+two+three < two+three+four)
+print(f"increasing: {p2}")
+# 433 is too low
+# 1497
+# for i in  zip(nums, nums[1:], nums[2:], nums[3:]):
+#     print(i)
+#     (6569, 6563, 6591, 6601)
+#     (6563, 6591, 6601, 6632)
